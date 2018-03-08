d69157afbe6821634eb6d75ba82768a4a82e21ef
中野 <nakano@kmc.gr.jp>
2018-03-09 00:30:25 +0900

    RAMとROMの追加

--- SIPS4.v
+++ SIPS4.v
@@ -1,4 +1,23 @@
 module SIPS4 (
 	input wire clk // 50MHz input clock
 );
+reg [3:0]ram_wdata;
+reg [3:0]ram_waddr;
+reg [3:0]ram_raddr;
+wire [3:0]ram_rdata;
+reg ram_wen;
+RAM ram(
+	.clock(clk),
+	.data(ram_wdata),
+	.rdaddress(ram_raddr),
+	.wraddress(ram_waddr),
+	.wren(ram_wen),
+	.q(ram_rdata));
+reg [3:0]PC;
+wire [15:0]instruction;
+ROM rom(.address(PC),.clock(clk),.q(instruction));
+
+always @(posedge clk)begin
+
+end
 endmodule
---
+++ greybox_tmp/greybox_tmp/mgl1e1.v
@@ -0,0 +1,76 @@
+//altdpram CBX_SINGLE_OUTPUT_FILE="ON" INDATA_ACLR="OFF" INDATA_REG="INCLOCK" INTENDED_DEVICE_FAMILY=""Cyclone V"" LPM_TYPE="altdpram" OUTDATA_ACLR="OFF" OUTDATA_REG="UNREGISTERED" RDADDRESS_ACLR="OFF" RDADDRESS_REG="INCLOCK" RDCONTROL_ACLR="OFF" RDCONTROL_REG="UNREGISTERED" READ_DURING_WRITE_MODE_MIXED_PORTS="NEW_DATA" USE_EAB="OFF" WIDTH=4 WIDTH_BYTEENA=1 WIDTHAD=4 WRADDRESS_ACLR="OFF" WRADDRESS_REG="INCLOCK" WRCONTROL_ACLR="OFF" WRCONTROL_REG="INCLOCK" data inclock q rdaddress wraddress wren
+//VERSION_BEGIN 17.1 cbx_mgl 2017:10:25:18:08:29:SJ cbx_stratixii 2017:10:25:18:06:53:SJ cbx_util_mgl 2017:10:25:18:06:53:SJ  VERSION_END
+// synthesis VERILOG_INPUT_VERSION VERILOG_2001
+// altera message_off 10463
+
+
+
+// Copyright (C) 2017  Intel Corporation. All rights reserved.
+//  Your use of Intel Corporation's design tools, logic functions 
+//  and other software and tools, and its AMPP partner logic 
+//  functions, and any output files from any of the foregoing 
+//  (including device programming or simulation files), and any 
+//  associated documentation or information are expressly subject 
+//  to the terms and conditions of the Intel Program License 
+//  Subscription Agreement, the Intel Quartus Prime License Agreement,
+//  the Intel FPGA IP License Agreement, or other applicable license
+//  agreement, including, without limitation, that your use is for
+//  the sole purpose of programming logic devices manufactured by
+//  Intel and sold by Intel or its authorized distributors.  Please
+//  refer to the applicable agreement for further details.
+
+
+
+//synthesis_resources = altdpram 1 
+//synopsys translate_off
+`timescale 1 ps / 1 ps
+//synopsys translate_on
+module  mgl1e1
+	( 
+	data,
+	inclock,
+	q,
+	rdaddress,
+	wraddress,
+	wren) /* synthesis synthesis_clearbox=1 */;
+	input   [3:0]  data;
+	input   inclock;
+	output   [3:0]  q;
+	input   [3:0]  rdaddress;
+	input   [3:0]  wraddress;
+	input   wren;
+
+	wire  [3:0]   wire_mgl_prim1_q;
+
+	altdpram   mgl_prim1
+	( 
+	.data(data),
+	.inclock(inclock),
+	.q(wire_mgl_prim1_q),
+	.rdaddress(rdaddress),
+	.wraddress(wraddress),
+	.wren(wren));
+	defparam
+		mgl_prim1.indata_aclr = "OFF",
+		mgl_prim1.indata_reg = "INCLOCK",
+		mgl_prim1.intended_device_family = ""Cyclone V"",
+		mgl_prim1.lpm_type = "altdpram",
+		mgl_prim1.outdata_aclr = "OFF",
+		mgl_prim1.outdata_reg = "UNREGISTERED",
+		mgl_prim1.rdaddress_aclr = "OFF",
+		mgl_prim1.rdaddress_reg = "INCLOCK",
+		mgl_prim1.rdcontrol_aclr = "OFF",
+		mgl_prim1.rdcontrol_reg = "UNREGISTERED",
+		mgl_prim1.read_during_write_mode_mixed_ports = "NEW_DATA",
+		mgl_prim1.use_eab = "OFF",
+		mgl_prim1.width = 4,
+		mgl_prim1.width_byteena = 1,
+		mgl_prim1.widthad = 4,
+		mgl_prim1.wraddress_aclr = "OFF",
+		mgl_prim1.wraddress_reg = "INCLOCK",
+		mgl_prim1.wrcontrol_aclr = "OFF",
+		mgl_prim1.wrcontrol_reg = "INCLOCK";
+	assign
+		q = wire_mgl_prim1_q;
+endmodule //mgl1e1
+//VALID FILE
