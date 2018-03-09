f504a345a3647d776e0094cb2a63ad64094a4230
Suikaba <suibaka@hotmail.co.jp>
2018-03-10 01:53:38 +0900

    rename test.v -> SIPS4_test.v

--- SIPS4_test.v
+++ SIPS4_test.v
@@ -1,15 +1,20 @@
 `timescale 100ps/100ps
-module test();
+
+module SIPS4_test();
+reg        clk;
+wire [7:0] LED;
+reg  [3:0] slide;
+reg  [1:0] button;
+
 parameter STEP = 1000;
-reg clk;
-wire [7:0]LED;
-reg [3:0]slide;
-reg [1:0]button;
+
 SIPS4 cpu(.clk(clk),.slide(slide),.button(button),.LED(LED));
+
 always #(STEP/2)clk=~clk;
 initial begin
 	clk=0;
 	slide=0;
 	button=0;
 end
+
 endmodule
